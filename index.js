3ed6a14c1bbc193247feed228ab80af981554f68
gincode <vishalkamboj9211@gmail.com>
2023-06-03 23:55:12 +0530

    added upload photos feature and retrieve

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import multer from 'multer'
 
 mongoose
   .connect("mongodb://127.0.0.1:27017", { dbName: "backenf" })
@@ -17,12 +18,11 @@ const UserSchema = mongoose.Schema({
   email: String,
   password: String,
 });
-const User = mongoose.model("User", UserSchema);
-
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const upload = multer({ dest: 'uploads/' });
 
 app.set("view engine", "ejs");
 const isAuthenticated = async (req, res, next) => {
@@ -38,6 +38,48 @@ const isAuthenticated = async (req, res, next) => {
     res.redirect("/login");
   }
 };
+
+const User = mongoose.model("User", UserSchema);
+
+
+const photoSchema = new mongoose.Schema({
+  filename: String,
+  contentType: String,
+  uploadedAt: { type: Date, default: Date.now }
+});
+const Photo = mongoose.model('Photo', photoSchema);
+// Handle the POST request to upload a photo
+app.get('/photos', async (req, res) => {
+  try {
+    const photos = await Photo.find();
+    res.json(photos);
+  } catch (error) {
+    console.error('Failed to retrieve photos:', error);
+    res.status(500).json({ error: 'Failed to retrieve photos' });
+  }
+});
+app.post('/photos', upload.single('photo'), async (req, res) => {
+  const { filename, mimetype, buffer } = req.file;
+
+  try {
+    // Create a new instance of the Photo model
+    const photo = new Photo({
+      filename,
+      contentType:mimetype ,
+    });
+
+    // Save the photo to MongoDB
+    const savedPhoto = await photo.save();
+    console.log('Photo uploaded successfully');
+    res.json({ message: 'Photo uploaded successfully' ,
+  filename,mimetype});
+  } catch (error) {
+    console.error('Failed to save the photo:', error);
+    res.status(500).json({ error: 'Failed to save the photo' });
+  }
+});
+
+
 app.get("/", isAuthenticated, (req, res) => {
   res.render("logout");
 });
@@ -75,7 +117,7 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+  const {  email, password } = req.body;
 
   let user = await User.findOne({ email });
   if (user) {
@@ -84,7 +126,7 @@ app.post("/register", async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   user = await User.create({
-    name,
+    
     email,
     password: hashedPassword,
   });
@@ -104,6 +146,15 @@ app.get("/logout", (req, res) => {
     expires: new Date(Date.now()),
   });
   res.redirect("/");
+});
+//getall users
+app.get('/users', async (req, res) => {
+  try {
+    const allusers = await User.find({});
+    res.json(allusers);
+  } catch (err) {
+res.json({err})
+  }
 });
 app.listen(3000, () => {
   console.log("port 3000");
